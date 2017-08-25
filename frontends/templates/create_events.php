01f52b7bb69e6c73d055761325643dc2ffbda1ac
Oyetoke Toby <oyetoketoby@yahoo.com>
2017-08-25 13:49:50 +0100

    Add files via upload

---
+++ frontends/templates/create_events.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lovents | Your Personal Assistant Events Creator</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ frontends/templates/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lovents | Your Personal Assistant Events Creator</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="header">
+	<div class="container-fluid">
+		<div class="logo">
+			<img src="" class="img-logo">
+		</div>
+		<div class="nav">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Create</a></li>
+				<li><a href="">Login</a></li> |
+				<li><a href="">Register</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="banner-wrapper">
+		<div class="banner">
+			
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
